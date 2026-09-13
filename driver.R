e0b0ce02896b014b6b7aa38c65a911c9c1ab6b14
otool23 <otool23@wfu.edu>
2026-09-13 13:26:48 -0400

    Update driver.R

--- driver.R
+++ driver.R
@@ -1,10 +1,7 @@
-getwd()
-setwd("/Users/mcdonaldlab1/Desktop/R/R_week3/")
-
 if (!file.exists('data')) dir.create('data')
 if (!file.exists('src')) dir.create('src')
 if (!file.exists('results')) dir.create('results')
-if (!file.exists('docs')) dirs.create('docs')
+if (!file.exists('docs')) dir.create('docs')
 
 source('src/EditDataframe.R')
 source('src/Analysis.R')
