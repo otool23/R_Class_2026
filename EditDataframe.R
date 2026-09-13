36c432169300c3d083c0c6a5ff365e8ff7f97361
otool23 <otool23@wfu.edu>
2026-09-13 13:27:16 -0400

    Update EditDataframe.R

--- EditDataframe.R
+++ EditDataframe.R
@@ -1,19 +1,25 @@
-df.t1 <- read.csv('/Users/mcdonaldlab1/Desktop/R/R_week3/tempExperiment-raw.csv')
+df.t1 <- read.csv('tempExperiment-raw.csv')
+
 head(df.t1)
 str(df.t1)
 
 a <- strsplit(as.character(df.t1$temp), split = '-')
 newvar <- matrix(unlist(a), ncol = 2, byrow = TRUE)
+
 head(newvar)
 
 df.t2 <- df.t1
+
 df.t2$temp <- newvar[,1]
 df.t2$pop <- newvar[,2]
+
 head(df.t2)
 
 df.t2$temp <- sub('ten', '10', df.t2$temp)
 df.t2$temp <- sub('twenty', '20', df.t2$temp)
+
 df.t2$pop <- sub('population ', 'pop', df.t2$pop)
+
 str(df.t2)
 
 df.t2$temp <- factor(df.t2$temp)
@@ -21,4 +27,4 @@ df.t2$pop <- factor(df.t2$pop)
 
 str(df.t2)
 
-write.csv(df.t2, '/Users/mcdonaldlab1/Desktop/R/R_week3/tempExperiment_v2.csv')
+write.csv(df.t2, 'data/tempExperiment_v2.csv')
